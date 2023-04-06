d62cc630e0eb0b3bd75293c2877eea6fa72c04c0
Jan David <jdno@jdno.dev>
2023-04-06 23:31:26 +0200

    Create model for configuration (#9)
    The configuration stores the user's defaults in a file in the
    repository, which makes it easier to run flowcrafter's CLI.

--- src/flowcrafter/src/lib.rs
+++ src/flowcrafter/src/lib.rs
@@ -1,12 +1,14 @@
 extern crate core;
 
 pub use self::{
+    configuration::Configuration,
     error::Error,
     job::{Job, JobBuilder},
     library::{Library, LibraryBuilder},
     workflow::{Workflow, WorkflowBuilder},
 };
 
+mod configuration;
 mod error;
 mod job;
 mod library;

f0beeebe04f2d9146825e005517de93e6ff04e46
Jan David <jdno@jdno.dev>
2023-04-06 23:11:35 +0200

    Create model for configuration
    The configuration stores the user's defaults in a file in the
    repository, which makes it easier to run flowcrafter's CLI.

---
+++ src/flowcrafter/src/configuration.rs
@@ -0,0 +1,65 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Deserialize, Serialize)]
+pub struct Configuration {
+    library: LibraryConfiguration,
+}
+
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Deserialize, Serialize)]
+pub struct LibraryConfiguration {
+    repository: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use indoc::indoc;
+
+    use super::*;
+
+    const CONFIG: &str = indoc! {r#"
+        library:
+          repository: owner/name
+    "#};
+
+    #[test]
+    fn trait_deserialize() {
+        let config: Configuration = serde_yaml::from_str(CONFIG).unwrap();
+        assert_eq!(
+            LibraryConfiguration {
+                repository: "owner/name".to_string()
+            },
+            config.library
+        );
+    }
+
+    #[test]
+    fn trait_serialize() {
+        let config = Configuration {
+            library: LibraryConfiguration {
+                repository: "owner/name".to_string(),
+            },
+        };
+
+        let yaml = serde_yaml::to_string(&config).unwrap();
+
+        assert_eq!(CONFIG, yaml);
+    }
+
+    #[test]
+    fn trait_send() {
+        fn assert_send<T: Send>() {}
+        assert_send::<Configuration>();
+    }
+
+    #[test]
+    fn trait_sync() {
+        fn assert_sync<T: Sync>() {}
+        assert_sync::<Configuration>();
+    }
+
+    #[test]
+    fn trait_unpin() {
+        fn assert_unpin<T: Unpin>() {}
+        assert_unpin::<Configuration>();
+    }
+}

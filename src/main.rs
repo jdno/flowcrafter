61e04793bf8ac9eb29ab4d9e70003da625a090d6
Jan David <jdno@jdno.dev>
2023-05-25 09:26:13 +0200

    Pass project to commands (#19)
    Instead of being passed the current working directory as a path,
    commands now get a reference to a `Project` struct. This makes it easier
    to reuse functionality, e.g. to find the root directory of a project.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use anyhow::Context;
 use clap::Parser;
 
-use flowcrafter::cli::{Cli, Commands};
+use flowcrafter::cli::{Cli, Commands, Project};
 use flowcrafter::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let cli = Cli::parse();
-    let cwd = std::env::current_dir().context("failed to detect current directory")?;
 
-    Commands::execute(&cli.command, &cwd).await
+    let cwd = std::env::current_dir().context("failed to detect current directory")?;
+    let project = Project::find(cwd)?;
+
+    Commands::execute(&cli.command, &project).await
 }

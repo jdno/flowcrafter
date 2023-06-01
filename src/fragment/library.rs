8b5eac8067c273ecae0a6720e4f7473354e51445
Jan David <jdno@jdno.dev>
2023-06-01 07:38:40 +0200

    Add lifetime to library trait (#22)
    Adding a lifetime to the library trait enables implementations of the
    trait to take only references to the workflow and job names.

--- src/fragment/library.rs
+++ src/fragment/library.rs
@@ -4,7 +4,7 @@ use crate::error::Error;
 use crate::fragment::Fragment;
 
 #[async_trait]
-pub trait FragmentLibrary {
-    async fn workflow(&self, name: &str) -> Result<Fragment, Error>;
-    async fn job(&self, workflow: &str, name: &str) -> Result<Fragment, Error>;
+pub trait FragmentLibrary<'a> {
+    async fn workflow(&self, name: &'a str) -> Result<Fragment, Error>;
+    async fn job(&self, workflow: &'a str, name: &'a str) -> Result<Fragment, Error>;
 }
